929f238085a050cbe166f299424c8d02c22373ee
Ajaybhaskar-Athi <ajaybhaskar.athi@gmail.com>
2024-11-12 23:53:24 +0530

    Amaravati Essentials

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,7 @@ app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 app.use('/api/upload', uploadRouter);
 
-const publicFolder = path.join(__dirname, 'public');
+const publicFolder = path.join(__dirname, '../public');
 app.use(express.static(publicFolder));
 
 app.get('*', (req, res) => {
